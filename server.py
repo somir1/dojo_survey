0d1a19f293dd642add312747dbef33fa378a3f4a
somir1 <leonsomir@gmail.com>
2021-07-12 15:48:22 -0500

    dojo survey with some bootstrap

---
+++ server.py
@@ -0,0 +1,17 @@
+  
+from flask import Flask, render_template, request, redirect, flash
+from flask.globals import session
+app = Flask(__name__)
+app.secret_key = "surveystuff"
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/userresults', methods=["POST"])
+def showSurveyinfo():
+    return render_template("results.html", name = request.form["name"], dojo = request.form["dojo_location"], favlan = request.form["favlang"], comments = request.form["comments"])
+
+if __name__=="__main__":
+    
+    app.run(debug=True) 
